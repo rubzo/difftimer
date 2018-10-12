0ab66c2b5685b0bd72e7e14a067443d948f546c0
Stephen Kyle <jimmykane@gmail.com>
2018-10-12 21:24:02 +0100

    refactoring

--- difftimer.js
+++ difftimer.js
@@ -1,3 +1,6 @@
+//
+// tstate: Timer State
+//
 let tstates = {
   select_delta: 1,
   select_max: 2,
@@ -7,6 +10,9 @@ let tstates = {
   times_up: 6
 };
 
+//
+// dstate: Differential State
+//
 let dstates = {
   not_ready: 1,
   players_equal: 2,
@@ -19,8 +25,13 @@ let dstates = {
   players_drew: 9
 };
 
+//
+// Our global state.
+//
 let tstate = tstates.select_delta;
 let dstate = dstates.not_ready;
+let prev_tstate = null;
+let prev_dstate = null;
 let diff_limit_ms = 0;
 let max_limit_ms = 0;
 let no_max = false;
@@ -29,74 +40,95 @@ let player2_time = 0;
 let current_differential = 0;
 let last_check_time = null;
 
+// Handle for timer callback.
 let setinterval_handle = null;
 
-let div_menu_select_delta = null;
-let div_menu_select_max = null;
-let div_timer_view = null;
-let div_player1_button = null;
-let div_player2_button = null;
-let div_player1_warning = null
-let div_player2_warning = null
-let div_diff_timer = null
-let div_player1_time = null;
-let div_player2_time = null;
-let div_restart_button = null;
-let div_pause_button = null;
+// References to divs we need to preload so we can change them dynamically.
+let div_ids_to_preload = [
+  "menu-select-delta",
+  "menu-select-max",
+  "timer-view",
+  "player1-button",
+  "player2-button",
+  "player1-warning",
+  "player2-warning",
+  "diff-timer1",
+  "diff-timer2",
+  "player1-time",
+  "player2-time",
+  "restart-button",
+  "pause-button"
+];
+let ds = {};
+
+function preload_divs() {
+  div_ids_to_preload.forEach(function (n) {
+    ds[n] = document.getElementById(n);
+  });
+}
+
+function set_only_class(element, class_name) {
+  element.classList = [];
+  element.classList.add(class_name);
+}
+
+function hide(element) {
+  element.classList.remove("visible");
+  element.classList.add("hidden");
+}
+
+function reveal(element) {
+  element.classList.remove("hidden");
+  element.classList.add("visible");
+}
 
 function show_players_equal() {
-  div_player1_warning.classList = [];
-  div_player1_warning.classList.add("equal-player");
-  div_player1_warning.children[0].innerHTML = "";
-  div_player2_warning.classList = [];
-  div_player2_warning.classList.add("equal-player");
-  div_player2_warning.children[0].innerHTML = "";
+  set_only_class(ds["player1-warning"], "equal-player");
+  set_only_class(ds["player2-warning"], "equal-player");
+  hide(ds["player1-warning"].children[0]);
+  hide(ds["player2-warning"].children[0]);
 }
 
 function show_player1_behind() {
-  div_player1_warning.classList = [];
-  div_player1_warning.classList.add("behind-player");
-  div_player1_warning.children[0].innerHTML = "▲▲▲▲▲";
-  div_player2_warning.classList = [];
-  div_player2_warning.classList.add("ahead-player");
-  div_player2_warning.children[0].innerHTML = "";
+  set_only_class(ds["player1-warning"], "behind-player");
+  set_only_class(ds["player2-warning"], "ahead-player");
+  reveal(ds["player1-warning"].children[0]);
+  hide(ds["player2-warning"].children[0]);
 }
 
 function show_player2_behind() {
-  div_player1_warning.classList = [];
-  div_player1_warning.classList.add("ahead-player");
-  div_player1_warning.children[0].innerHTML = "";
-  div_player2_warning.classList = [];
-  div_player2_warning.classList.add("behind-player");
-  div_player2_warning.children[0].innerHTML = "▼▼▼▼▼";
+  set_only_class(ds["player1-warning"], "ahead-player");
+  set_only_class(ds["player2-warning"], "behind-player");
+  hide(ds["player1-warning"].children[0]);
+  reveal(ds["player2-warning"].children[0]);
 }
 
 function clear_current_player_button_state() {
   if (tstate == tstates.player1_active) {
-    div_player1_button.classList.remove("active-player");
-    div_player2_button.classList.remove("waiting-player");
+    ds["player1-button"].classList.remove("active-player");
+    ds["player2-button"].classList.remove("waiting-player");
   } else if (tstate == tstates.player2_active) {
-    div_player1_button.classList.remove("waiting-player");
-    div_player2_button.classList.remove("active-player");
+    ds["player1-button"].classList.remove("waiting-player");
+    ds["player2-button"].classList.remove("active-player");
   }
 }
 
 function show_player1_lost() {
   clear_current_player_button_state();
-  div_player1_button.classList.add("lost-player");
-  div_player2_button.classList.add("won-player");
+  ds["player1-button"].classList.add("lost-player");
+  ds["player2-button"].classList.add("won-player");
 }
 
 function show_player2_lost() {
   clear_current_player_button_state();
-  div_player1_button.classList.add("won-player");
-  div_player2_button.classList.add("lost-player");
+  ds["player1-button"].classList.add("won-player");
+  ds["player2-button"].classList.add("lost-player");
 }
 
 function show_players_drew() {
   clear_current_player_button_state();
-  div_player1_button.classList.add("lost-player");
-  div_player2_button.classList.add("lost-player");
+  ds["player1-button"].classList.add("lost-player");
+  ds["player2-button"].classList.add("lost-player");
 }
 
 function convert_ms_to_minsec_string(t) {
@@ -115,23 +147,22 @@ function is_player2_behind() {
 }
 
 function update_time_display() {
-  div_player1_time.innerHTML = convert_ms_to_minsec_string(player1_time);
-  div_player2_time.innerHTML = convert_ms_to_minsec_string(player2_time);
-  let diff_string = convert_ms_to_minsec_string(Math.abs(current_differential)) + " (" + convert_ms_to_minsec_string(diff_limit_ms) + ")";
+  ds["player1-time"].innerHTML = convert_ms_to_minsec_string(player1_time);
+  ds["player2-time"].innerHTML = convert_ms_to_minsec_string(player2_time);
+
+  let diff_string = convert_ms_to_minsec_string(Math.abs(current_differential)) + " (" +
+      convert_ms_to_minsec_string(diff_limit_ms) + ")";
   let diff_sign1 = "";
-  if (is_player1_behind()) {
-    diff_sign1 = "+";
-  } else if (is_player2_behind()) {
-    diff_sign1 = "-";
-  }
   let diff_sign2 = "";
   if (is_player1_behind()) {
+    diff_sign1 = "+";
     diff_sign2 = "-";
   } else if (is_player2_behind()) {
+    diff_sign1 = "-";
     diff_sign2 = "+";
   }
-  div_diff_timer1.innerHTML = diff_sign1 + diff_string;
-  div_diff_timer2.innerHTML = diff_sign2 + diff_string;
+  ds["diff-timer1"].innerHTML = diff_sign1 + diff_string;
+  ds["diff-timer2"].innerHTML = diff_sign2 + diff_string;
 }
 
 function update_times() {
@@ -184,29 +215,28 @@ function update_times() {
   update_time_display();
 }
 
+//
+// State changers.
+//
 function change_tstate(target_state) {
   if (target_state == tstates.select_max) {
-    div_menu_select_delta.classList.remove("visible");
-    div_menu_select_delta.classList.add("hidden");
-    div_menu_select_max.classList.remove("hidden");
-    div_menu_select_max.classList.add("visible");
+    hide(ds["menu-select-delta"]);
+    reveal(ds["menu-select-max"]);
   } else if (target_state == tstates.waiting_for_first_move) {
-    div_menu_select_max.classList.remove("visible");
-    div_menu_select_max.classList.add("hidden");
-    div_timer_view.classList.remove("hidden");
-    div_timer_view.classList.add("visible");
+    hide(ds["menu-select-max"]);
+    reveal(ds["timer-view"]);
   } else if (target_state == tstates.player1_active) {
-    div_player2_button.classList.remove("active-player");
-    div_player1_button.classList.remove("waiting-player");
-    div_player2_button.classList.add("waiting-player");
-    div_player1_button.classList.add("active-player");
+    ds["player2-button"].classList.remove("active-player");
+    ds["player1-button"].classList.remove("waiting-player");
+    ds["player2-button"].classList.add("waiting-player");
+    ds["player1-button"].classList.add("active-player");
     // TODO: sound
     update_times();
   } else if (target_state == tstates.player2_active) {
-    div_player1_button.classList.remove("active-player");
-    div_player2_button.classList.remove("waiting-player");
-    div_player1_button.classList.add("waiting-player");
-    div_player2_button.classList.add("active-player");
+    ds["player1-button"].classList.remove("active-player");
+    ds["player2-button"].classList.remove("waiting-player");
+    ds["player1-button"].classList.add("waiting-player");
+    ds["player2-button"].classList.add("active-player");
     // TODO: sound
     update_times();
   } else if (target_state == tstates.times_up) {
@@ -218,6 +248,7 @@ function change_tstate(target_state) {
   if (tstate == tstates.waiting_for_first_move) {
     setinterval_handle = setInterval(update_times, 200);
   }
+  prev_tstate = tstate;
   tstate = target_state;
 }
 
@@ -235,9 +266,13 @@ function change_dstate(target_state) {
   } else if (target_state == dstates.players_drew) {
     show_players_drew();
   }
+  prev_dstate = dstate;
   dstate = target_state;
 }
 
+//
+// Button listeners.
+//
 function delta_button_listener(e) {
   let id_string = e.currentTarget.id;
   let delta_secs = id_string.slice(11);
@@ -285,6 +320,9 @@ function pause_button_listener(e) {
 
 }
 
+//
+// Setup functions.
+//
 function add_listeners_to_child_buttons(div, listener) {
   let container = div.getElementsByClassName("button-container")[0];
   for (let i = 0; i < container.children.length; i++) {
@@ -293,27 +331,13 @@ function add_listeners_to_child_buttons(div, listener) {
 }
 
 function initial_setup() {
-  div_timer_view = document.getElementById("timer-view");
-
-  div_menu_select_delta = document.getElementById("menu-select-delta");
-  add_listeners_to_child_buttons(div_menu_select_delta, delta_button_listener);
-  div_menu_select_max = document.getElementById("menu-select-max");
-  add_listeners_to_child_buttons(div_menu_select_max, max_button_listener);
-
-  div_player1_button = document.getElementById("player1-button");
-  div_player1_button.addEventListener("click", player1_button_listener);
-  div_player2_button = document.getElementById("player2-button");
-  div_player2_button.addEventListener("click", player2_button_listener);
-  div_player1_warning = document.getElementById("player1-warning");
-  div_player2_warning = document.getElementById("player2-warning");
-  div_diff_timer1 = document.getElementById("diff-timer1");
-  div_diff_timer2 = document.getElementById("diff-timer2");
-  div_player1_time = document.getElementById("player1-time");
-  div_player2_time = document.getElementById("player2-time");
-  div_restart_button = document.getElementById("restart-button");
-  div_restart_button.addEventListener("click", restart_button_listener);
-  div_pause_button = document.getElementById("pause-button");
-  div_pause_button.addEventListener("click", pause_button_listener);
+  preload_divs();
+  add_listeners_to_child_buttons(ds["menu-select-delta"], delta_button_listener);
+  add_listeners_to_child_buttons(ds["menu-select-max"], max_button_listener);
+  ds["player1-button"].addEventListener("click", player1_button_listener);
+  ds["player2-button"].addEventListener("click", player2_button_listener);
+  ds["restart-button"].addEventListener("click", restart_button_listener);
+  ds["pause-button"].addEventListener("click", pause_button_listener);
 };
 
 window.onload = initial_setup;
